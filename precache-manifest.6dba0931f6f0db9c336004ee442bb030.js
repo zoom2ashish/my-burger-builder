fa8a745ed57c2302818fa53b9840194635be5449
Ashish Patel <ashishp@vmware.com>
2020-03-10 11:22:11 -0700

    Updates

---
+++ precache-manifest.6dba0931f6f0db9c336004ee442bb030.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ef7601f8b1e16e3d17781b2c60287a",
+    "url": "/my-burger-builder/index.html"
+  },
+  {
+    "revision": "bb9d6bd8ad536b7f8ee5",
+    "url": "/my-burger-builder/static/css/main.5f38d148.chunk.css"
+  },
+  {
+    "revision": "299c385ca96407b8f775",
+    "url": "/my-burger-builder/static/js/2.51026ed9.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/my-burger-builder/static/js/2.51026ed9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb9d6bd8ad536b7f8ee5",
+    "url": "/my-burger-builder/static/js/main.11b30c86.chunk.js"
+  },
+  {
+    "revision": "7a3a5959a031182aaec2",
+    "url": "/my-burger-builder/static/js/runtime-main.cbc79805.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/my-burger-builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
